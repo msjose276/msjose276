3eeea172c54a93912493e42797f32b3e97449083
Mateus Jose <mateus.s.o.jose@gmail.com>
2022-05-01 22:39:24 -0500

    finished task list application

---
+++ TaskList/taskList.js
@@ -0,0 +1,23 @@
+document.getElementById("taskList").value ="";
+var taskList = window.localStorage.getItem('taskList');
+document.getElementById("taskList").value = taskList;
+
+function addTask(){
+    var inputVal = document.getElementById("task").value;
+    var listOfTasks = document.getElementById("taskList").value;
+
+    if(document.getElementById("taskList").value.length == 0)
+        document.getElementById("taskList").value =inputVal;
+    else
+        document.getElementById("taskList").value = listOfTasks+"\n"+inputVal;
+    
+    document.getElementById("task").value ="";
+
+    window.localStorage.setItem('taskList', document.getElementById("taskList").value);
+}
+
+function clearTasks(){
+    document.getElementById("taskList").value ="";
+    window.localStorage.removeItem('taskList');
+
+}
