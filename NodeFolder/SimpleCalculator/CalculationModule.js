a386ae3afdd50206c4975a458ec326178518b8c0
Mateus Jose <mateus.s.o.jose@gmail.com>
2022-05-09 22:41:03 -0500

    made final modification to the Simple Calculator

--- NodeFolder/SimpleCalculator/CalculationModule.js
+++ NodeFolder/SimpleCalculator/CalculationModule.js
@@ -1,62 +1,51 @@
 
+const ResultPage1 = `<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+        <title>Simple Calculator</title>
+        <link href="./SimpleCalculator.css" type="text/css" rel="stylesheet">
+    </head>
+    <body>
+        <h2>Simple Calculator</h2>
+        <div class="form">
+            <div class="container">
+                <div class="col-50">
+                    <h3>The Answer is `;
+
+var  ResultPage2= `
+                    </h3>
+                </div>
+
+                <div class="col-50">                   
+                    <a href="./SimpleCalculator.html"><button class="button" type="">Another calculation</button></a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html> `;
 
 exports.addition = function (req,res,vals) {
-    var sum = parseInt(vals.first) + parseInt(vals.second);
-
-    res.write(`<!DOCTYPE html>
-        <html>
-        <head><meta charset=\"utf-8\"/>
-        <title>Calculator Web Site</title>
-        </head>
-        <body>
-        <p>The sum is: ${String(sum)}</p>
-        </body>
-        </html> ` ); 
+    var result = parseInt(vals.first) + parseInt(vals.second);
+    res.write(ResultPage1  + String(result) + ResultPage2);
     return res.end();
 };
 
 exports.subtruction = function (req,res,vals) {
-    var sum = parseInt(vals.first) - parseInt(vals.second);
-
-    res.write(`<!DOCTYPE html>
-        <html>
-        <head><meta charset=\"utf-8\"/>
-        <title>Calculator Web Site</title>
-        </head>
-        <body>
-        <p>The sum is: ${String(sum)}</p>
-        </body>
-        </html> ` ); 
+    var result = parseInt(vals.first) - parseInt(vals.second);
+    res.write(ResultPage1  + String(result) + ResultPage2);
     return res.end();
 };
 
 exports.division = function (req,res,vals) {
-    var sum = parseInt(vals.first) / parseInt(vals.second);
-
-    res.write(`<!DOCTYPE html>
-        <html>
-        <head><meta charset=\"utf-8\"/>
-        <title>Calculator Web Site</title>
-        </head>
-        <body>
-        <p>The sum is: ${String(sum)}</p>
-        </body>
-        </html> ` ); 
+    var result = parseFloat(vals.first) / parseFloat (vals.second);
+    res.write(ResultPage1  + String(result) + ResultPage2);
     return res.end();
 };
 
 exports.multiplication = function (req,res,vals) {
-    var sum = parseInt(vals.first) * parseInt(vals.second);
-
-    res.write(`<!DOCTYPE html>
-        <html>
-        <head><meta charset=\"utf-8\"/>
-        <title>Calculator Web Site</title>
-        </head>
-        <body>
-        <p>The sum is: ${String(sum)}</p>
-        </body>
-        </html> ` ); 
+    var result = parseInt(vals.first) * parseInt(vals.second);
+    res.write(ResultPage1  + String(result) + ResultPage2);
     return res.end();
 };
     
